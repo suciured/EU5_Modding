ce41b5e9e51ff19bdbf079be5d09551e069cb5d8
suciured <suciured@gmail.com>
2025-12-28 18:11:32 -0600

    Update Sync_AuthorityLedger.cmd

--- Workspace/Sync_AuthorityLedger.cmd
+++ Workspace/Sync_AuthorityLedger.cmd
@@ -1,4 +1,7 @@
 @echo off
 echo Syncing Authority Ledger...
+
+REM %~dp0 = directory this CMD is in (with trailing backslash)
 powershell -ExecutionPolicy Bypass -File "%~dp0Sync_AuthorityLedger.ps1"
+
 pause
